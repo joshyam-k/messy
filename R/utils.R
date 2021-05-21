dd261381e304839f1cb27e1ebf018f5bb82c7b00
joshyam-k <yamamojo@reed.edu>
2021-05-21 15:45:24 -0700

    utilities function

--- R/utils.R
+++ R/utils.R
@@ -1,1 +1,11 @@
 
+binary_strings <- function(n, digits = c('0','1')) {
+
+  if (n <= 0) return(matrix(0,0,0))
+  if (n == 1) return(matrix(digits, 2))
+
+  x <- binary_strings(n - 1)
+
+  rbind(cbind(digits[1], x), cbind(digits[2], x))
+
+}
