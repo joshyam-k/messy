f50f17d45ca54d3ab94c66d8b87116224d0aaa6b
joshyam-k <yamamojo@reed.edu>
2021-05-28 12:49:59 -0700

    cleaning

--- R/messy_string.R
+++ R/messy_string.R
@@ -13,7 +13,6 @@
 
 new_strings <- function(string) {
 
-  # separate string into its individual words
 
   if (stringr::str_detect(string, "-")) {
 
@@ -23,13 +22,9 @@ new_strings <- function(string) {
 
   words <- stringr::str_to_lower(stringr::str_split(string, " ")[[1]])
 
-  # create bistrings
-
   n <- length(words)
 
   binary_tibble <- as.data.frame(binary_strings(n), .name_repair = "unique")
-
-  # create placeholder tibbles to be filled with modified strings
 
   full_word_cap <- binary_tibble
   first_letter_cap <- binary_tibble
@@ -38,7 +33,7 @@ new_strings <- function(string) {
 
     for (col in 1:n) {
 
-      if (binary_tibble[row, col] == '0') {
+      if (binary_tibble[row, col] == "0") {
 
         first_letter_cap[row, col] <- words[col]
         full_word_cap[row, col] <- words[col]
@@ -55,12 +50,9 @@ new_strings <- function(string) {
     }
   }
 
-  # right now each word is a different column so we'd like to the unite the columns with a space as the separator
 
   first_letter_cap <- tidyr::unite(first_letter_cap, phrase, 1:ncol(first_letter_cap), sep = " ")
   full_word_cap <- tidyr::unite(full_word_cap, phrase, 1:ncol(full_word_cap), sep = " ")
-
-  # remove any duplicates
 
   final_phrases <- dplyr::distinct(rbind(first_letter_cap, full_word_cap), .keep_all = T)
 
@@ -98,8 +90,6 @@ new_strings <- function(string) {
 
   }
 
-  # turn our dataframe into a vector where each object is a string
-
   unlist(final_phrases, use.names = F)
 
 }
@@ -113,7 +103,7 @@ messy_string <- function(string) {
     hyphen_bits <- binary_strings(length(locs))
     hyphen_options <- 2**(length(locs))
 
-    string2 <- stringr::str_replace_all(string, "-", " ")
+    string_hyphen_rm <- stringr::str_replace_all(string, "-", " ")
 
 
     item_add <- function(x, loc, item) {
@@ -125,14 +115,13 @@ messy_string <- function(string) {
     final_hyphens <- c()
     for (i in 1:hyphen_options) {
 
-      final_phrases_piece <- new_strings(string2)
+      final_phrases_piece <- new_strings(string_hyphen_rm)
 
       for (col in 1:length(locs)) {
 
         if (hyphen_bits[i, col] == "0") {
 
           final_phrases_piece <- purrr::map_chr(final_phrases_piece, ~item_add(.x, locs[col], " "))
-
 
         } else {
 
@@ -147,8 +136,6 @@ messy_string <- function(string) {
 
 
     }
-
-
 
     ret <- final_hyphens
 
